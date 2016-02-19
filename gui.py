a354b47f6f7ee6b803ea6edd8e572d54130c8136
rambokiller <awajikachi@gmail.com>
2016-02-19 14:23:33 +0530

    Graphics Program in Python

---
+++ gui.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#Now this is what I'm talking about
+from graphics import *
+win = GraphWin('myEye')
+
+leftEye = Circle(Point(80,50),10)
+leftEye.setFill('yellow')
+leftEye.setOutline('red')
+
+myI=input()
